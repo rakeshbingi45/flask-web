4a5aca00999523268347cb2563bfbe4210c1a043
Rakesh B <116814069+rakeshbingi45@users.noreply.github.com>
2025-05-19 09:17:40 +0000

    Major Changes

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask( __name__)
 
-Electronics = [
-  {
-    'id' : '1',
-    'name' : 'Swift',
-    'price' : '100000',
-  },
-  {
-    'id' : '2',
-    'name' : 'Dell Pro',
-    'price' : '120000',
-  },
-  {
-    'id' : '3',
-    'name' : 'HP Victus',
-    'price' : '130000',
-  }
-]
-
 @app.route('/')
-def Hello_World():
-  return render_template('home.html',laptops=Electronics)
+def index():
+  with open('static/data/featured_products.json') as f:
+    featured_products = json.load(f)
+  return render_template('index.html',featured_products = featured_products)
 
+@app.route('/categories')
+def categories():
+    return render_template('categories.html')
+
+@app.route('/categories/mobiles')
+def mobiles():
+    return render_template('mobiles.html')
+
+@app.route('/categories/laptops')
+def laptops():
+    return render_template('laptops.html')
+
+@app.route('/categories/audio')
+def audio():
+    return render_template('audio.html')
+
+@app.route('/categories/monitors')
+def monitors():
+    return render_template('monitors.html')
+
+@app.route('/categories/consoles')
+def consoles():
+    return render_template('consoles.html')
+
+@app.route('/smartwatches')
+def smartwatches():
+    return render_template('smartwatches.html')
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
